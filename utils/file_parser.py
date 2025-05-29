ad0b64cb406d72f6034c931c373d213f836a666a
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-05-29 16:56:33 -0400

    Fix client wrapper

--- utils/file_parser.py
+++ utils/file_parser.py
@@ -1,14 +1,23 @@
 import tree_sitter
 import tree_sitter_python as tspython
-from lang_conf import BaseLangConf, PythonLangConf
 from openai import OpenAI
+
+from utils.lang_conf import BaseLangConf, PythonLangConf
 
 lang = tree_sitter.Language(tspython.language())
 parser = tree_sitter.Parser(lang)
 
-client = OpenAI(
-    base_url="https://api.metisai.ir/openai/v1"
-)
+
+class OpenAIClient():
+    _client = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._client:
+            return cls._client
+        cls._client = OpenAI(
+            base_url="https://api.metisai.ir/openai/v1"
+        )
+        return cls._client
 
 
 class FileParser():
@@ -16,11 +25,13 @@ class FileParser():
     file_bytes: bytes = None
     tree: tree_sitter.Tree = None
     lang_conf: BaseLangConf = None
+    client: OpenAI = None
 
     def __init__(self, path):
         self.path = path
         self.parser = parser
         self.lang_conf = PythonLangConf
+        self.client = OpenAIClient()
 
     def analyze_file(self):
         with open(self.path) as file:
@@ -31,10 +42,9 @@ class FileParser():
             self.generate_tags(self.tree.root_node)
 
     def generate_method_doc(self, node: tree_sitter.Node):
-        source = self.file_bytes[node.start_byte:node.end_byte].decode('utf-8')
-        print(source)
-        print(node.text.decode('utf-8'))
-        response = client.responses.create(
+        # source = self.file_bytes[node.start_byte:node.end_byte].decode('utf-8')
+        source = node.text.decode('utf-8')
+        response = self.client.responses.create(
             model="gpt-4.1-nano",
             input=[
                 {"role": "system", "content": f"""
@@ -46,6 +56,6 @@ class FileParser():
         print(self.lang_conf.getMethodName(node), response.output_text)
 
     def generate_tags(self, root: tree_sitter.Node):
-        for node in root.children():
+        for node in root.children:
             if self.lang_conf.isDocNeeded(node):
                 self.generate_method_doc(node)
