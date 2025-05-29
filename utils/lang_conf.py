8f60cf04db197ccf754754a588893d7eeb8ddb03
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-05-29 18:16:21 -0400

    Add new required methods

--- utils/lang_conf.py
+++ utils/lang_conf.py
@@ -1,5 +1,7 @@
 import tree_sitter
 
+from models import ProjectKnowledgeBase
+
 
 class BaseLangConf():
     @classmethod
@@ -13,6 +15,18 @@ class BaseLangConf():
                 return child.text.decode()
         return None
 
+    @classmethod
+    def generateIdentifier(cls, project: ProjectKnowledgeBase, node: tree_sitter.Node):
+        raise NotImplementedError()
+
+    @classmethod
+    def isWorthyScope(cls, project: ProjectKnowledgeBase, node: tree_sitter.Node):
+        raise NotImplementedError()
+
+    @classmethod
+    def generateNodePath(cls, node: tree_sitter.Node):
+        raise NotImplementedError()
+
 
 class PythonLangConf(BaseLangConf):
     @classmethod
@@ -25,3 +39,21 @@ class PythonLangConf(BaseLangConf):
             if child.type == 'identifier':
                 return child.text.decode()
         return None
+
+    @classmethod
+    def generateIdentifier(cls, project: ProjectKnowledgeBase, file_path: str, node: tree_sitter.Node):
+        return f"{project.name}:{file_path}:{cls.generateNodePath(node)}"
+
+    @classmethod
+    def isWorthyScope(cls, node: tree_sitter.Node):
+        return node.type == 'function_definition' or node.type == 'class_definition'
+
+    @classmethod
+    def generateNodePath(cls, node: tree_sitter.Node):
+        sections = []
+        while node:
+            if cls.isWorthyScope(node):
+                sections.append(cls.getMethodName(node))
+
+            node = node.parent
+        return '.'.join(reversed(sections))
