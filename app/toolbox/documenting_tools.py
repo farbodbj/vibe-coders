76bc08d0033cd660a17c30e9d61566430c3bb139
farbod <farbod.bjary82@gmail.com>
2025-05-29 22:31:49 +0330

    bs

--- app/toolbox/documenting_tools.py
+++ app/toolbox/documenting_tools.py
@@ -7,17 +7,6 @@ class DocGen:
     def __init__(self, llm: ChatOpenAI):
         self._llm = llm
         
-    def _generate_doc(self, node_source: str, node_name: str):
-        prompt = f'''Document the function {node_name} in detail, including its purpose, input parameters, output values, and any exceptions it may throw.
-        Provide clear examples and usage instructions to help developers understand how to use the function effectively.
-        
-        function body: {node_source}
-        '''
-        return self._llm.invoke(
-            input=prompt
-        ).content
-        
-        
     def _get_method_name(node: tree_sitter.Node):
         for child in node.children:
             if child.type == 'identifier':
@@ -28,7 +17,15 @@ class DocGen:
     def generate_doc(self, ts: tree_sitter.Node, file_bytes)->str:
         method_source = file_bytes[ts.start_byte:ts.end_byte].decode('utf-8')  
         method_name = self._get_method_name(ts)
-        return self._generate_doc(method_name, method_source)
+        
+        prompt = f'''Document the function {method_name} in detail, including its purpose, input parameters, output values, and any exceptions it may throw.
+        Provide clear examples and usage instructions to help developers understand how to use the function effectively.
+        
+        function body: {method_source}
+        '''
+        return self._llm.invoke(
+            input=prompt
+        ).content
     
     
     def generate_method(self, ts: tree_sitter.Node, file_bytes, indt: str = ""):
