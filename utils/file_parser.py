c848c9bd1b66c9792bbe221e6fde5c3e688df490
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-05-29 18:17:14 -0400

    Save/load knowledge base for project

--- utils/file_parser.py
+++ utils/file_parser.py
@@ -2,6 +2,7 @@ import tree_sitter
 import tree_sitter_python as tspython
 from openai import OpenAI
 
+from models import Node, ProjectKnowledgeBase
 from utils.lang_conf import BaseLangConf, PythonLangConf
 
 lang = tree_sitter.Language(tspython.language())
@@ -27,11 +28,26 @@ class FileParser():
     lang_conf: BaseLangConf = None
     client: OpenAI = None
 
-    def __init__(self, path):
+    file_ref: Node = None
+
+    def __init__(self, project: ProjectKnowledgeBase, path: str):
         self.path = path
         self.parser = parser
         self.lang_conf = PythonLangConf
         self.client = OpenAIClient()
+
+        self.project = project
+
+        self.file_ref = Node(
+            gid=f"{project.name}:{path}",
+            identifier=path,
+            file=path,
+            full_path=path,
+            path=path,
+
+            node_type="file",
+        )
+        project.nodes[self.file_ref.gid] = self.file_ref
 
     def analyze_file(self):
         with open(self.path) as file:
@@ -53,7 +69,18 @@ class FileParser():
                 {"role": "user", "content": source},
             ]
         )
-        print(self.lang_conf.getMethodName(node), response.output_text)
+
+        saved_node = Node(
+            gid=self.lang_conf.generateIdentifier(
+                self.project, self.path, node),
+            identifier=self.lang_conf.getMethodName(node),
+            file=self.path,
+            path=self.lang_conf.generateNodePath(node),
+            node_type=node.type,
+            short_doc=response.output_text
+        )
+
+        self.project.nodes[saved_node.gid] = saved_node
 
     def generate_tags(self, root: tree_sitter.Node):
         for node in root.children:
